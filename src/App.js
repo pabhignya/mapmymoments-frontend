e7f96f5761916f6b84adad4d8b1040b02cdf12a3
pabhignya <abhignya.p@northeastern.edu>
2025-10-26 15:31:44 -0400

    Initialize React app with Tailwind setup

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and Tailwind
         </a>
       </header>
     </div>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"], // scans React files
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
